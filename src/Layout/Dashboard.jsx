5222409c8a9c45bfbc6152118a39acb0806fbbd6
programarreza <programarreza.com>
2023-11-15 12:34:05 +0600

    updated dashboard and item delete system

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,41 +1,54 @@
 import { FaCalendar, FaCartShopping } from "react-icons/fa6";
 import { NavLink, Outlet } from "react-router-dom";
+import useCart from "../hooks/useCart";
 
 const Dashboard = () => {
+  const [cart] = useCart();
+
+
   return (
     <div className="flex">
       {/* dashboard side bar */}
-      <div className="w-64 min-h-screen bg-[#D1A054]">
+      <div className="w-64 min-h-screen bg-[#D1A054] p-8">
         <ul>
           <li className="flex items-center gap-2">
-			<FaCartShopping/>
+            <FaCartShopping />
             <NavLink to={"/dashboard/userHome"}>User Home</NavLink>
           </li>
           <li className="flex items-center gap-2">
-			<FaCalendar/>
+            <FaCalendar />
             <NavLink to={"/dashboard/reservation"}>Reservation</NavLink>
           </li>
           <li className="flex items-center gap-2">
-			<FaCalendar/>
+            <FaCalendar />
             <NavLink to={"/dashboard/payment"}>PAYMENT HISTORY</NavLink>
           </li>
           <li className="flex items-center gap-2">
-			<FaCartShopping/>
-            <NavLink to={"/dashboard/cart"}>My Cart</NavLink>
+            <FaCartShopping />
+            <NavLink to={"/dashboard/cart"}>My Cart ({cart.length})</NavLink>
           </li>
           <li className="flex items-center gap-2">
-			<FaCartShopping/>
+            <FaCartShopping />
             <NavLink to={"/dashboard/review"}>Add Review</NavLink>
           </li>
           <li className="flex items-center gap-2">
-			<FaCartShopping/>
+            <FaCartShopping />
             <NavLink to={"/dashboard/bookings"}>My Bookings</NavLink>
+          </li>
+          <div className="divider"></div>
+          <li className="flex items-center gap-2">
+            <FaCartShopping />
+            <NavLink to={"/"}> Home</NavLink>
+          </li>
+          <li className="flex items-center gap-2">
+            <FaCartShopping />
+            <NavLink to={"/order/salads"}> Menu</NavLink>
           </li>
         </ul>
       </div>
 
       {/* dashboard content */}
-      <div className="flex-1">
+      <div className="flex-1 p-8">
         <Outlet></Outlet>
       </div>
     </div>
--- src/Pages/Dashboard/Cart/Cart.jsx
+++ src/Pages/Dashboard/Cart/Cart.jsx
@@ -1,10 +1,104 @@
+import { FaIdeal, FaTrash } from "react-icons/fa6";
+import useCart from "../../../hooks/useCart";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const Cart = () => {
-	return (
-		<div>
-			cart
-		</div>
-	);
+  const [cart, refetch] = useCart();
+  const totalPrice = cart.reduce((total, item) => total + item.price, 0);
+  const axiosSecure = useAxiosSecure();
+
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/carts/${id}`).then((res) => {
+          console.log(res.data);
+          if (res.data.deletedCount) {
+            refetch();
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your file has been deleted.",
+              icon: "success",
+            });
+          }
+        });
+      }
+    });
+  };
+
+  return (
+    <>
+      <div className="flex  justify-evenly">
+        <h2 className="text-3xl">Items: {cart.length}</h2>
+        <h2 className="text-3xl">Total Price: {totalPrice}</h2>
+        <button className="btn btn-primary">Pay</button>
+      </div>
+
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>
+                <label>SL</label>
+              </th>
+              <th>ITEM IMAGE</th>
+              <th>ITEM NAME</th>
+              <th>PRICE</th>
+              <th>ACTION</th>
+              <th>ACTION</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            {cart.map((item, i) => (
+              <tr key={item._id}>
+                <th>
+                  <label>{i + 1}</label>
+                </th>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle w-12 h-12">
+                        <img
+                          src={item?.image}
+                          alt="Avatar Tailwind CSS Component"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </td>
+                <td>{item?.name}</td>
+                <td>${item?.price}</td>
+
+                <th>
+                  <button className="btn btn-ghost btn-xl">
+                    <FaIdeal className="text-red-500"></FaIdeal>
+                  </button>
+                </th>
+                <th>
+                  <button
+                    onClick={() => handleDelete(item?._id)}
+                    className="btn btn-ghost btn-xl"
+                  >
+                    <FaTrash className="text-red-500"></FaTrash>
+                  </button>
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
 };
 
 export default Cart;
